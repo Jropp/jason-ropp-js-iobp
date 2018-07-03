81a9b313b42a9d0a0c9b09d5995c081f489a6638
Jason Ropp <jason.ropp@banno.com>
2018-07-03 18:07:33 -0400

    second lookover

--- src/api/user-database.js
+++ src/api/user-database.js
@@ -1,59 +1,74 @@
-export {database};
+export { database };
 
 let database = (() => {
-	function getUsers() {
-		return JSON.parse(localStorage.getItem('onboardProjectUsers')) || [];
-	}
+  function getUsers() {
+    return JSON.parse(localStorage.getItem("onboardProjectUsers")) || [];
+  }
 
-	function saveUser(user) {
-				let users = getUsers();
-        let index = getIdIndex(user.id, users);
-
-        const isNewUser = index === -1;
-
-        if (isNewUser) {
-          user.id = createNewUserId();
-          users.splice(0, 0, user);
-        } else {
-          users.splice(index, 1, user);
-        }
-
-        updateLocalStorage(users);
+  function getUser(userId) {
+    let users = getUsers();
+    users.forEach(user => {
+      if (user.id === userId) {
+        return user;
       }
+    });
+  }
 
-	function getIdIndex(id, users) {
-        return (users) ? users.findIndex(val => val.id === id) : -1;
-      }
+  function saveUser(user) {
+    let users = getUsers();
+    let index = getIdIndex(user.id);
 
-	function updateLocalStorage(users){
-		localStorage.setItem('onboardProjectUsers', JSON.stringify(users));
-	}
+    const isNewUser = index === -1;
+    if (isNewUser) {
+      user.id = createNewUserId();
+      users.splice(0, 0, user);
+    } else {
+      users.splice(index, 1, user);
+    }
 
-	function createNewUserId() {
-        let id = '';
-        let asciiCharsLowest = 33;
-        let asciiCharsHighest = 123;
-        let idLength = 9;
+    updateLocalStorage(users);
+  }
 
-        for (let i = 0; i < idLength; i++) {
-          let charCode = Math.floor(Math.random() * (asciiCharsHighest - asciiCharsLowest)) + asciiCharsLowest;
-          id += String.fromCharCode(charCode);
-        }
+  function deleteUser(user) {
+    let users = getUsers();
+    users.splice(getIdIndex(user.id, users), 1);
+    updateLocalStorage(users);
+  }
 
-        let doesIdExist = getIdIndex(id) === -1;
-        return (doesIdExist) ? id : createNewUserId();
-      }
+  function deleteUsers() {
+    localStorage.setItem("onboardProjectUsers", JSON.stringify([]));
+  }
 
-		function deleteUser(user) {
-				let users = getUsers();
-        users.splice(getIdIndex(user.id, users), 1);
-        updateLocalStorage(users);
-      }
+  function getIdIndex(id) {
+    let users = getUsers();
+    return users ? users.findIndex(val => val.id === id) : -1;
+  }
 
-	return {
-		getUsers: getUsers,
-		deleteUser: deleteUser,
-		saveUser: saveUser
-	}
+  function updateLocalStorage(users) {
+    localStorage.setItem("onboardProjectUsers", JSON.stringify(users));
+  }
+
+  function createNewUserId() {
+    let id = "";
+    let asciiCharsLowest = 33;
+    let asciiCharsHighest = 123;
+    let idLength = 9;
+
+    for (let i = 0; i < idLength; i++) {
+      let charCode =
+        Math.floor(Math.random() * (asciiCharsHighest - asciiCharsLowest)) +
+        asciiCharsLowest;
+      id += String.fromCharCode(charCode);
+    }
+
+    let doesIdExist = getIdIndex(id) !== -1;
+    return doesIdExist ? createNewUserId() : id;
+  }
+
+  return {
+    getUsers: getUsers,
+    saveUser: saveUser,
+    deleteUser: deleteUser,
+    deleteUsers: deleteUsers
+  };
 })();
-
