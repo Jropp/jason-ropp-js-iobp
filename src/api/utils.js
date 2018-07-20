80e6a9648a001eb06bd55e3148869e65baba7dcb
Unknown <jasonropp4tl@gmail.com>
2018-07-20 09:49:57 -0400

    deleted phoneInputWatcher
    I moved checking phone input into form watcher, so it calls the same function on-blur that the whole form calls on input. This will make it easier to bring in other checkers into one place

---
+++ src/api/utils.js
@@ -0,0 +1,45 @@
+export { phone };
+
+let phone = (() => {
+  function insertPhoneSymbols(phoneNumber) {
+    let phoneArray = phoneNumber.split("");
+    phoneArray = phoneArray.reverse();
+    let domesticSymbols = [
+      { name: "dash", loc: 4, text: "-" },
+      { name: "spaceLoc", loc: 8, text: " " },
+      { name: "leftParLoc", loc: 9, text: ")" },
+      { name: "rightParLoc", loc: 13, text: "(" }
+    ];
+
+    domesticSymbols.forEach(symbol => {
+      phoneArray.splice(symbol.loc, 0, symbol.text);
+    });
+
+    let domesticNumCharLength = 14;
+    let interSpaceLoc = 14;
+    if (phoneArray.length > domesticNumCharLength) {
+      phoneArray.splice(interSpaceLoc, 0, " ");
+      let internationalNumPre = "+";
+      phoneArray.push(internationalNumPre);
+    }
+
+    phoneArray = phoneArray.reverse();
+    return phoneArray.join("");
+  }
+
+  function checkPhoneInput(phone) {
+    let phoneDigits = phone.replace(/\D/g, "");
+    let internationalDigitMax = 13;
+    let domesticDigitLength = 10;
+
+    return (
+      phoneDigits.length >= domesticDigitLength &&
+      phoneDigits.length <= internationalDigitMax
+    );
+  }
+
+  return {
+    insertPhoneSymbols: insertPhoneSymbols,
+    checkPhoneInput: checkPhoneInput
+  };
+})();
