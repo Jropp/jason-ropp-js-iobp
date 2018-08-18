c8395f57e75c9cf3105eecec6b05d81a6f8ab929
Unknown <jasonropp4tl@gmail.com>
2018-08-18 11:37:02 -0400

    Requested syntax changes.
    Collin requested some small syntax changes in my loader. This is also a snapshot before merging the two loaders.

--- build-tools/jha-design-components-js-loader.js
+++ build-tools/jha-design-components-js-loader.js
@@ -1,5 +1,5 @@
 const PolymerDot = /window\..*=\sclass\sextends\sPolymer\.Element/;
-const classFormatRegex = /\=\sclass\sextends\sPolymer.Element\s\{/;
+const classFormatRegex = /\=\sclass\sextends\sPolymer\.Element\s\{/;
 const classFormatFix = `= class extends PolymerElement {`;
 const importPolymer =
   'import { Element as PolymerElement } from "@banno/polymer/polymer-element.js"';
@@ -8,7 +8,7 @@ module.exports = content => {
   if (content.match(PolymerDot)) {
     let fixedClassDeclaration = content.replace(
       classFormatRegex,
-      `${classFormatFix}`
+      classFormatFix
     );
 
     let importAdded = `${importPolymer}\n${fixedClassDeclaration};`;
