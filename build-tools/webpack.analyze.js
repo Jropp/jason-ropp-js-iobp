3a0b11c30d143e3d4a44888f3e6729f60408d06f
Unknown <jasonropp4tl@gmail.com>
2018-09-17 15:15:33 -0400

    Analyze fix

--- build-tools/webpack.analyze.js
+++ build-tools/webpack.analyze.js
@@ -2,9 +2,12 @@ const webpack = require("webpack");
 const path = require("path");
 const webpackBundleAnalyzer = require("webpack-bundle-analyzer")
   .BundleAnalyzerPlugin;
+const MinifyPlugin = require("babel-minify-webpack-plugin");
 
 module.exports = () => {
   return {
-    plugins: [new webpackBundleAnalyzer()]
+    plugins: [
+      new webpackBundleAnalyzer(),
+      new MinifyPlugin()]
   };
 };
